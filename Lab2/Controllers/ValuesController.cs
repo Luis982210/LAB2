4c9d2173dc959f4e84ceaac9ad5eebe6e4ee00e5
Luis982210 <46784371+Luis982210@users.noreply.github.com>
2020-02-21 22:12:05 -0600

    Add files via upload

---
+++ Lab2/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lab2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IGaseosaSettings _agua;
+        public ValuesController(IGaseosaSettings moviesettings)
+        {
+            _agua = moviesettings;
+        }
+        //localhost:51626/api/Values/GetWithParam/?nombre=""
+        [HttpGet("GetWithParam", Name = "Get")]
+        public ActionResult<object> Get(string nombre)
+        {
+            object tempo = _agua.GetOne(nombre);
+            return tempo;
+        }
+
+        [HttpPost]
+        public ActionResult<Gaseosa> Create(Gaseosa gas)
+        {            
+            _agua.Create(gas);
+            Disck (gas);
+            return gas;
+        }
+        private void Disck(Gaseosa elementeo)
+        {
+            var jsonPatientList = JsonConvert.SerializeObject(elementeo);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "GaseosasData/");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            var UploadedFile = Request.Form.Files[0];
+            if (UploadedFile!=null)
+            {
+                string ServerDirectory = Directory.GetCurrentDirectory();
+                string UpFilesDirectoryPath = Path.Combine(ServerDirectory, "GaseosasData/");
+                string WServerPath = UpFilesDirectoryPath + Path.GetFileName(UploadedFile.FileName.Replace(".txt", "ZigZagEncoded.txt"));
+            }
+        }
+    }
+}
---
+++ Lab2/Models/GaseosaSettings.cs
@@ -0,0 +1,39 @@
+﻿using Lab2.Arbol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    public class GaseosaSettings : IGaseosaSettings
+    {
+        public ArbolAsterisco<string, Gaseosa> arbol { get; set; }
+
+        public GaseosaSettings()
+        {
+            arbol = new ArbolAsterisco<string, Gaseosa>(4);// el 4 es el grado 
+        }
+        public void Create(Gaseosa nuevo)
+        {
+            arbol.Insertar(nuevo.Nombre,nuevo);
+        }
+
+        public List<Gaseosa> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Object GetOne(string nombre)
+        {            
+            return arbol.Search(nombre);
+        }
+    }
+    public interface IGaseosaSettings
+    {
+        ArbolAsterisco<string, Gaseosa> arbol { get; set; }
+        Object GetOne(string nombre);
+        List<Gaseosa> Get();
+        void Create(Gaseosa nuevo);
+    }
+}
